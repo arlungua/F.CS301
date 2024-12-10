3555ae98e1828544ef029d98e7aa61496180e8fe
Arlungua B221910016 <164838596+arlungua@users.noreply.github.com>
2024-12-10 20:45:44 +0800

    Create 12-4.java

---
+++ Lab 12/12-4.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
+
+        for (int[] prereq : prerequisites) {
+            isPrerequisite[prereq[0]][prereq[1]] = true;
+        }
+
+        for (int k = 0; k < numCourses; k++) {
+            for (int i = 0; i < numCourses; i++) {
+                for (int j = 0; j < numCourses; j++) {
+                    if (isPrerequisite[i][k] && isPrerequisite[k][j]) {
+                        isPrerequisite[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int[] query : queries) {
+            result.add(isPrerequisite[query[0]][query[1]]);
+        }
+
+        return result;
+    }
+}
