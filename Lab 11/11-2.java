05cc0cd3621eaff26bfce021f46daf9d7473a92c
Arlungua B221910016 <164838596+arlungua@users.noreply.github.com>
2024-12-10 20:40:37 +0800

    Update 11-2.java

--- Lab 11/11-2.java
+++ Lab 11/11-2.java
@@ -1,1 +1,24 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int openLow = 0, openHigh = 0;
 
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                openLow++;
+                openHigh++;
+            } else if (c == ')') {
+                openLow = Math.max(0, openLow - 1);
+                openHigh--;
+            } else { // c == '*'
+                openLow = Math.max(0, openLow - 1);
+                openHigh++;
+            }
+
+            if (openHigh < 0) {
+                return false;
+            }
+        }
+
+        return openLow == 0;
+    }
+}
