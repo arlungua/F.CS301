6978630a7a53aaf86c25dcd9e2b3b8f3fd5c9d17
Arlungua B221910016 <164838596+arlungua@users.noreply.github.com>
2024-12-10 19:44:41 +0800

    Update binary_search.py

--- Lab 2/binary_search.py
+++ Lab 2/binary_search.py
@@ -1,1 +1,12 @@
-
+class BinarySearch:
+    def search(self, array, low, high, key):
+        if high >= low:
+            mid = (high + low) // 2
+            if array[mid] == key:
+                return mid
+            elif array[mid] > key:
+                return self.search(array, low, mid - 1, key)
+            else:
+                return self.search(array, mid + 1, high, key)
+        else:
+            return -1
