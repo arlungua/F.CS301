1e4c108d3023f6932a10bd67c707cff28fea2602
Arlungua B221910016 <164838596+arlungua@users.noreply.github.com>
2024-12-10 19:47:26 +0800

    Create merge_sort.py

---
+++ Lab 2/merge_sort.py
@@ -0,0 +1,31 @@
+class MergeSort:
+    def sort(self, array):
+        if len(array) > 1:
+            mid = len(array) // 2
+            left_half = array[:mid]
+            right_half = array[mid:]
+
+            self.sort(left_half)
+            self.sort(right_half)
+
+            i = j = k = 0
+            while i < len(left_half) and j < len(right_half):
+                if left_half[i] < right_half[j]:
+                    array[k] = left_half[i]
+                    i += 1
+                else:
+                    array[k] = right_half[j]
+                    j += 1
+                k += 1
+
+            while i < len(left_half):
+                array[k] = left_half[i]
+                i += 1
+                k += 1
+
+            while j < len(right_half):
+                array[k] = right_half[j]
+                j += 1
+                k += 1
+
+        return array
